bbacbf66cd1bacef7345243d664fc4c144b1b3d9
romain <romain@hol.ly>
2015-12-26 08:57:55 +1100

    Role selector.

--- authorization_drupal_roles/src/Plugin/authorization/consumer/DrupalRolesConsumer.php
+++ authorization_drupal_roles/src/Plugin/authorization/consumer/DrupalRolesConsumer.php
@@ -20,6 +20,29 @@ use Drupal\authorization\Consumer\ConsumerPluginBase;
 class DrupalRolesConsumer extends ConsumerPluginBase {
 
   public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $role_options = array();
+    $roles = user_roles(TRUE);
+    foreach ( $roles as $key => $role ) {
+      if ( $key != 'authenticated' ) {
+        $role_options[$key] = $role->label();
+      }
+    }
+    // @TODO an AJAX callback to get the bundles based on the entity
+    $form['role'] = array(
+      '#type' => 'select',
+      '#title' => t('Role'),
+      '#options' => $role_options,
+      '#default_value' => $this->configuration['role'],
+      '#required' => TRUE,
+      '#description' => 'Choose the Drupal role to apply to the user.',
+      '#ajax' => array(
+        'trigger_as' => array('name' => 'drupalroleconsumer_role'),
+        'callback' => '::buildAjaxDrupalRoleConsumerConfigForm',
+        'wrapper' => 'authorization-consumer-config-form',
+        'method' => 'replace',
+        'effect' => 'fade',
+      ),
+    );
     return $form;
   }
 
