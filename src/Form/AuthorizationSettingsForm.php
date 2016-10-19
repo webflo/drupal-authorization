e36df07a0ab08412f370194129aa28f2e7881c13
Florian Weber <florian.weber@ueberbit.de>
2016-10-19 19:31:43 +0200

    Issue #2751699 by grahl, Ramdas Gaikar, webflo: Make messages optional

---
+++ src/Form/AuthorizationSettingsForm.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\authorization\AuthorizationSettingsForm
+ */
+
+namespace Drupal\authorization\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Configure authorization settings for this site.
+ */
+class AuthorizationSettingsForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'authorization_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'authorization.settings',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('authorization.settings');
+
+    $form['authorization_message'] = array(
+      '#type' => 'checkbox',
+      '#title' => $this->t('If enabled, all authorization and notification messages will be shown to the user.'),
+      '#default_value' => $config->get('authorization_message_on'),
+      );
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $this->config('authorization.settings')
+    ->set('authorization_message_on', $form_state->getValue('authorization_message'))
+    ->save();
+
+    parent::submitForm($form, $form_state);
+  }
+}
